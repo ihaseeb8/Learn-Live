f15b6aeb31d61202e8f66b0e09f70c7aedfd155b
ihaseeb8 <ihaseebullah.8@gmail.com>
2023-01-07 00:27:52 +0500

    Customized Admin Account Details

--- frontend/src/components/AdminAccountDetails.jsx
+++ frontend/src/components/AdminAccountDetails.jsx
@@ -1,6 +1,7 @@
 import React , {useState, useEffect} from 'react'
-import { Box, Heading, Text, Link } from '@chakra-ui/react'
+import { Box, Heading, Text, Flex, Avatar, Input, IconButton  } from '@chakra-ui/react'
 import axios from "axios"
+import theme from '../theme/theme'
 
 const AdminAccountDetails = () => {
 
@@ -34,28 +35,111 @@ const AdminAccountDetails = () => {
 
 
   return (
-    <Box p={5}>
-      <Heading as="h2" size="lg">
-        Account Details
-      </Heading>
-      <Text mt={4}>    
-        Here you can view and edit your account details.    
-      </Text>
-      <Text mt={5}>
-      Name: {name}
-      </Text>
-      <Text mt={5}>
-      Email: {email}
-      </Text>
-      <Text mt={5}>
-      Gender: {gender}
-      </Text>
-      <Text mt={5}>
-      Phone Number: {phoneno}
-      </Text>
+    
+    
+    <Box p={5} pt={0} px={0} mx='auto' textAlign={'center'} width={'100%'}>
+      <Box pt={4} pb={2} background='orange.500' borderTopRadius={'30px'}>
+        <Heading mb={4} color='white'>
+          Account Details
+        </Heading>
+        <Text mb={6} >
+          This page displays your account details and allows you to edit them.
+        </Text>
+      </Box>
 
-      {/* <Link  mt={5} to="/edit-account">Edit Account</Link> */}
+      <Box p={5} maxW="lg" mx="auto" textAlign={'start'} position={'relative'}>
+
+        <Box align="center" mb={4} mx='auto' px='auto' >
+          <Avatar size='2xl' src={`https://avatars.dicebear.com/v2/bottts/${name}.svg?`} />
+          <Heading fontSize="xl" color="orange.500">
+              {name}
+          </Heading>
+
+          {/* Add Implementation for Edit Account */}
+          <IconButton
+            position={'absolute'}
+            top={8}
+            right={8}
+            variant='outline'
+            aria-label="Edit account details"
+            icon= {<i class="fa-sharp fa-solid fa-user-pen"></i>}
+            color='orange'
+            colorScheme={'orange'}
+            size='lg'
+  
+          />
+        </Box>
+
+        <Box border={'1px solid orange'} borderRadius='20px' p={4} >
+
+          <Flex mb={2} align='center'>
+            <Text fontWeight="bold" color="orange.500" mr={2} >
+              Email:
+            </Text>
+            <Input value={email} 
+                   textAlign={'center'} 
+                   focusBorderColor='orange.700' 
+                   variant={'flushed'} 
+                   borderBottomColor='orange' 
+                   width={'60%'} 
+                   mr={0} ml='auto' />
+          </Flex>
+
+          <Flex mb={2} align='center'>
+            <Text fontWeight="bold" color="orange.500" mr={2} >
+              Gender:
+            </Text>
+            <Input value={gender} 
+                   textAlign={'center'} 
+                   focusBorderColor='orange.700' 
+                   variant={'flushed'} 
+                   borderBottomColor='orange' 
+                   width={'60%'} 
+                   mr={0} ml='auto' />
+          </Flex>
+
+          <Flex mb={2} align='center'>
+            <Text fontWeight="bold" color="orange.500" mr={2} >
+              Phone No:
+            </Text>
+            <Input value={phoneno} 
+                   textAlign={'center'} 
+                   focusBorderColor='orange.700' 
+                   variant={'flushed'} 
+                   borderBottomColor='orange' 
+                   width={'60%'} 
+                   mr={0} ml='auto' />
+          </Flex>
+
+        </Box>
+
+      </Box>
     </Box>
+
+    // <Box p={5}
+    //   >
+
+    //   <Heading as="h2" size="lg">
+    //     Account Details
+    //   </Heading>
+    //   <Text mt={4}>    
+    //     Here you can view and edit your account details.    
+    //   </Text>
+    //   <Text mt={5}>
+    //   Name: {name}
+    //   </Text>
+    //   <Text mt={5}>
+    //   Email: {email}
+    //   </Text>
+    //   <Text mt={5}>
+    //   Gender: {gender}
+    //   </Text>
+    //   <Text mt={5}>
+    //   Phone Number: {phoneno}
+    //   </Text>
+
+    //   {/* <Link  mt={5} to="/edit-account">Edit Account</Link> */}
+    // </Box>
   )
 }
 
--- frontend/src/theme/theme.js
+++ frontend/src/theme/theme.js
@@ -1,41 +1,53 @@
-import { Button, extendTheme, Input, theme as base, withDefaultColorScheme, withDefaultVariant } from "@chakra-ui/react";
-import {whiten} from '@chakra-ui/theme-tools'
+  import { Button, extendTheme, Input, theme as base, withDefaultColorScheme, withDefaultVariant } from "@chakra-ui/react";
+  import {whiten} from '@chakra-ui/theme-tools'
 
-const theme = extendTheme({
-    colors:{
-        brand: 
-        {
-          50: '#ffe2ec',
-          100: '#ffb3c5',
-          200: '#fc839f',
-          300: '#f95278',
-          400: '#f62252',
-          500: '#dd0939',
-          600: '#ad032c',
-          700: '#7c001e',
-          800: '#4d0012',
-          900: '#200005',
-        },
-        brand1: 
-        {
-          50: '#fff1db',
-          100: '#ffd8b0',
-          200: '#fbc081',
-          300: '#f9a652',
-          400: '#f68d22',
-          500: '#dd7309',
-          600: '#ac5a04',
-          700: '#7c3f02',
-          800: '#4c2500',
-          900: '#1f0b00',
-        }
-        
-    },
-    fonts: {
-        heading: `Montserrat, ${base.fonts.heading}`,
-        body: `Montserrat, ${base.fonts.heading}`,
-    },
-    }
-)
+  const theme = extendTheme({
+      colors:{
+          brand: 
+          {
+            50: '#ffe2ec',
+            100: '#ffb3c5',
+            200: '#fc839f',
+            300: '#f95278',
+            400: '#f62252',
+            500: '#dd0939',
+            600: '#ad032c',
+            700: '#7c001e',
+            800: '#4d0012',
+            900: '#200005',
+          },
+          brand1: 
+          {
+            50: '#fff1db',
+            100: '#ffd8b0',
+            200: '#fbc081',
+            300: '#f9a652',
+            400: '#f68d22',
+            500: '#dd7309',
+            600: '#ac5a04',
+            700: '#7c3f02',
+            800: '#4c2500',
+            900: '#1f0b00',
+          },
+          orange: {
+            50: "#FFF3E0",
+            100: "#FFE0B2",
+            200: "#FFCC80",
+            300: "#FFB74D",
+            400: "#FFA726",
+            500: "#FF9800",
+            600: "#FB8C00",
+            700: "#F57C00",
+            800: "#EF6C00",
+            900: "#E65100"
+          }
+          
+      },
+      fonts: {
+          heading: `Montserrat, ${base.fonts.heading}`,
+          body: `Montserrat, ${base.fonts.heading}`,
+      },
+      }
+  )
 
-export default theme
+  export default theme
